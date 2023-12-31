33c408aa0daddd80b70fe60eea77b1fbe95154b5
Nowress <mexiquekira@gmail.com>
2023-12-31 15:19:00 +0100

    first commit

--- urbansport/src/App.js
+++ urbansport/src/App.js
@@ -1,14 +1,17 @@
 import NavBar from "./compenents/NavBar"
+import Price from "./compenents/Price"
+import Footer from "./compenents/Footer"
+
 export default function App() {
   return (
     <main>
       <section><NavBar/></section>
       {/* <section><EventSection/></section>
       <section><Offres/></section>
-      <section><Schedule/></section>
+      {/* <section><Schedule/></section> */}
       <section><Price/></section>
-      <section><MapSection/></section>
-      <section><Footer/></section> */}
+      {/* {/* <section><MapSection/></section>  */}
+      <section><Footer/></section> 
     </main>
   )
 }
---
+++ urbansport/src/compenents/Price.jsx
@@ -0,0 +1,72 @@
+const Price =() =>{
+    return(
+       <div className=' mx-auto text-center w-full bg-[#151515] pt-28  ' >
+         <div> 
+        {/* partie de Youness*/}
+         <p className="font-black  text-red-700 text-ellipsis text-2xl ">Price Offres</p>
+      
+      </div>
+     
+      <div className="md:flex justify-between mx-16 block " >
+                <div className="border-2 border-slate-950 w-100 h-[250px]   p-4 m-10 skew-y-6 hover:bg-white group hover:text-black duration-500" > 
+                   <div className="-skew-y-6">
+                   <p className=" text-white p-3 font-bold text-xl	  group-hover:text-black ">SPORT DE COMBAT</p>
+                    <p className="inline text-red-800 text-xl font-black">1499 DH</p>
+                    <p className="font-black  text-gray-700  inline"> = 12 MOIS DE COMBAT</p>
+                  
+                    <p className="mb-3 text-xs text-yellow-500 ">MMA-BOXE-TAEKWONDO</p>
+                  <p className="inline text-red-800 text-xl font-black">1999 DH</p>
+                   <p className="font-black  text-gray-700 inline "> = 18 MOIS DE COMBAT</p>
+                    
+                    <p className="mb-3 text-xs  text-yellow-500 ">MMA-BOXE-TAEKWONDO</p>
+                  <p className="inline  text-red-800 text-xl font-black">2499 DH</p>
+                    <p className="font-black  text-gray-700 inline"> = 18 MOIS DE COMBAT</p>
+                    
+                    <p className="mb-3 text-xs  text-yellow-500 ">MMA-BOXE-TAEKWONDO</p>  
+                   
+                    </div>
+                   
+                </div>
+
+
+                <div className="border-2 border-slate-950 w-100 h-[250px]   p-4 m-10 skew-y-6 hover:bg-white group hover:text-black duration-500" > 
+                   <div className="-skew-y-6">
+                   <p className=" text-white p-3 font-bold text-xl	  group-hover:text-black ">ACADIMIE DE FOOTBALL</p>
+<div>                    <p className="inline  text-red-800 text-xl font-black">2499 DH </p>
+                    <p className="font-black  text-green-700 mb-3 inline">= LE FOOTBALL JUSQU'A LA FIN DE SAISON "(31 AOUT)"</p>
+</div>
+<div>                   <p className="inline  text-red-800 text-xl font-black">6249 DH </p>
+                   <p className="font-black  text-green-700 mb-3 inline">= 24 MOIS DE FOOTBALL</p>
+</div>                   
+                  
+                    </div>
+                   
+                </div>
+
+
+                <div className="border-2 border-slate-950 w-100 h-[250px]   p-4 m-10 skew-y-6 hover:bg-white group hover:text-black duration-500" > 
+                   <div className="-skew-y-6">
+                   <p className=" text-white p-3 font-bold text-xl	  group-hover:text-black ">FITNESS / MUSCULATION</p>
+<div className="mb-3    ">                    <p className="inline  text-red-800 text-xl font-black">1499 DH </p>
+                    <p className="font-black  font-montserrat  text-gray-700 mb-6  inline">= 24 MOIS DE FITNESS</p>
+</div>
+<div className="mb-3">                   <p className="inline  text-red-800 text-xl font-black">1999 DH </p>
+                   <p className="font-black  text-gray-700   inline">= 24 MOIS DE FITNESS</p>
+</div>
+<div className="mb-3">                   <p className="inline  text-red-800 text-xl font-black">2499 DH </p>
+                   <p className="font-black  text-gray-700 mb-6  inline">= 24 MOIS DE FITNESS</p>
+</div>
+                  
+                    </div>
+                   
+                </div>
+
+               
+
+
+
+      </div>
+       </div>
+    )
+}
+export default Price
