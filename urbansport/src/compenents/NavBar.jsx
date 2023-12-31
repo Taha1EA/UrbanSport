acec5fe4ba2b1f61438690662b426ca11b10f93f
TAHA1EA <elansari789taha@gmail.com>
2023-12-31 12:38:01 +0100

    add logo

--- urbansport/src/compenents/NavBar.jsx
+++ urbansport/src/compenents/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../App.css'
 import {hamburger} from '../assets/icons'
+import {logo} from '../assets/icons'
 import {navLinks} from '../assets/icons/index'
 function NavBar() {
   return (
     <header className="padding-x py-8 absolute z-10 w-full bg-black text-gray-300">
         <nav className='flex justify-between items-center max-container '>
-          <button href="#" className='ml-6'>Urbanport
-            {/* <img  alt='logo' width={130} height={40} /> */}
+          <button href="#" className='ml-6'>
+            <img src={logo} alt='logo' width={130} height={40} />
           </button>
           <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
           {navLinks.map((item) => (
