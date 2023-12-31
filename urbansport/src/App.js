0a92b0bdc59a9e1a3ec048918a45e4ad5a9ee322
TAHA1EA <elansari789taha@gmail.com>
2023-12-31 02:40:09 +0100

    first commit

---
+++ urbansport/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./compenents/NavBar"
+export default function App() {
+  return (
+    <main>
+      <section><NavBar/></section>
+      {/* <section><EventSection/></section>
+      <section><Offres/></section>
+      <section><Schedule/></section>
+      <section><Price/></section>
+      <section><MapSection/></section>
+      <section><Footer/></section> */}
+    </main>
+  )
+}
